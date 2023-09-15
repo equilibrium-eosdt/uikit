859484e97608f5c5f5def2c847c95c20a2a3a3d1
Vilmir Baikov <noname777228@rambler.ru>
2023-09-15 15:38:53 +0300

    refactor(uikit): Rename class names

--- constants/classnames.ts
+++ constants/classnames.ts
@@ -31,9 +31,9 @@ export const XL = "xl";
 // variants
 export const Action = "action";
 export const Title = "title";
-export const HeaderL = "header-l";
-export const HeaderM = "header-m";
-export const HeaderS = "header-s";
+export const HeaderL = "headerL";
+export const HeaderM = "headerM";
+export const HeaderS = "headerS";
 export const Body = "body";
 export const Caption = "caption";
 export const Secondary = "secondary";
