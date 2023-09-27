bd96ae395e90c9a911258752a424c6b5b13785b7
Vilmir Baikov <noname777228@rambler.ru>
2023-09-27 16:51:00 +0300

    fix(uikit): Button - move animation in pending class

--- components/button/styled.ts
+++ components/button/styled.ts
@@ -1,6 +1,17 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import * as Class from "../../constants/classnames";
 import { cssVar } from "../../util/style";
+
+const STRIPE_WIDTH = "56px";
+
+const animateButtonPendingBackground = keyframes`
+  0% {
+		transform: translateX(-${STRIPE_WIDTH});
+	}
+	100% {
+		transform: translateX(0);
+	}
+`;
 
 export const ButtonWrapper = styled.button`
   position: relative;
@@ -65,7 +76,25 @@ export const ButtonWrapper = styled.button`
     pointer-events: none;
 
     &:before {
+      content: " ";
+      transition: opacity 0.2s ease-in-out;
+      position: absolute;
       opacity: 1;
+      display: block;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: calc(100% + ${STRIPE_WIDTH});
+      background: repeating-linear-gradient(
+        135deg,
+        ${cssVar("--background-primary")} 25%,
+        ${cssVar("--background-primary")} 50%,
+        ${cssVar("--background-secondary")} 50%,
+        ${cssVar("--background-secondary")} 75%
+      );
+
+      background-size: ${STRIPE_WIDTH} ${STRIPE_WIDTH};
+      animation: ${animateButtonPendingBackground} 2s linear infinite;
     }
   }
 
