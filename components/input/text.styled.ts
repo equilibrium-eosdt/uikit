f6e9ba530bd9d9c90269ab986165eb4d9b3716a7
alissa_y <mintlol@mail.ru>
2023-09-27 15:45:58 +0300

    fix(uikit): Fix input width

--- components/input/text.styled.ts
+++ components/input/text.styled.ts
@@ -163,7 +163,7 @@ export const SuperfieldStyled = styled.div`
   input {
     border: none;
     outline: none;
-    min-width: 20px;
+    min-width: 12px;
     max-width: calc(100% - 40px);
     background: transparent;
     caret-color: var(--text-primary);
