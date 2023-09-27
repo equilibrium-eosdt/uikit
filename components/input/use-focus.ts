2b34acdab1361fef536e80c833332fd72d38f4c1
Artem1211 <31965650+Artem1211@users.noreply.github.com>
2023-09-27 14:48:36 +0400

    feat(uikit): Added input hooks (#45)
    * feat(uikit): Added input hooks
    
    * feat(uikit): Added extract props to the Superfield

---
+++ components/input/use-focus.ts
@@ -0,0 +1,17 @@
+import { useRef, useState } from "react"
+import { useClickOutside } from "../../hooks/use-outside-click";
+import { Focused } from "../../constants/classnames"
+
+export const useFocus = () => {
+    const ref = useRef<HTMLInputElement>(null)
+    const [inputFocused, setInputFocused] = useState(false);
+    useClickOutside(ref, () => {
+        setInputFocused(false)
+    });
+
+    return { 
+        onClick: () => setInputFocused(true),
+        [Focused]: inputFocused,
+        ref
+    }
+}
