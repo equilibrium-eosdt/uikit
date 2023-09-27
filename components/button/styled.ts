64dd0a40a5071487866e09d886a16665f04f472d
Vilmir Baikov <noname777228@rambler.ru>
2023-09-27 15:29:47 +0300

    fix(uikit): Button - delete before animation

--- components/button/styled.ts
+++ components/button/styled.ts
@@ -1,17 +1,6 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import * as Class from "../../constants/classnames";
 import { cssVar } from "../../util/style";
-
-const STRIPE_WIDTH = "56px";
-
-const animateButtonPendingBackground = keyframes`
-  0% {
-		transform: translateX(-${STRIPE_WIDTH});
-	}
-	100% {
-		transform: translateX(0);
-	}
-`;
 
 export const ButtonWrapper = styled.button`
   position: relative;
@@ -38,27 +27,6 @@ export const ButtonWrapper = styled.button`
     rect {
       transition: 0.3s ease-out;
     }
-  }
-  &:before {
-    content: " ";
-    transition: opacity 0.2s ease-in-out;
-    position: absolute;
-    opacity: 0;
-    display: block;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: calc(100% + ${STRIPE_WIDTH});
-    background: repeating-linear-gradient(
-      135deg,
-      ${cssVar("--background-primary")} 25%,
-      ${cssVar("--background-primary")} 50%,
-      ${cssVar("--background-secondary")} 50%,
-      ${cssVar("--background-secondary")} 75%
-    );
-
-    background-size: ${STRIPE_WIDTH} ${STRIPE_WIDTH};
-    animation: ${animateButtonPendingBackground} 2s linear infinite;
   }
 
   &.${Class.L} {
