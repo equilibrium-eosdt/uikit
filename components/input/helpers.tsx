20981c99b34e5e5bf22e796d4d22fcdf015e6b03
Alex D <pr0fedt@gmail.com>
2023-10-19 15:53:10 +0200

    add control and suggestion hotfixes to input field inner container

---
+++ components/input/helpers.tsx
@@ -0,0 +1,40 @@
+import cn from "classnames";
+
+import {
+  Suggestion,
+  Controls,
+  M,
+  Elevated,
+  Icon,
+} from "../../constants/classnames";
+
+import { DefaultProps } from "../../types/core";
+import { ComposeProps } from "../../types/util";
+import { constStrArray, divideBy } from "../../util/type";
+import Noop from "../helpers/noop";
+import { FieldContainer } from "./text.styled";
+
+const classes = constStrArray(Suggestion, Controls, M, Elevated, Icon);
+
+interface FieldContainerProps
+  extends DefaultProps,
+    Partial<ComposeProps<typeof classes, boolean>> {}
+
+export function FieldInnerContainer(_props: FieldContainerProps) {
+  const [classProps, props] = divideBy(_props, ...classes);
+
+  return (
+    <FieldContainer
+      className={cn(props.className, classProps)}
+      style={props.style}
+    >
+      {classProps.controls ? (
+        // resolves problem with controls used inside form
+        <button style={{ display: "none" }} />
+      ) : (
+        <Noop />
+      )}
+      {props.children}
+    </FieldContainer>
+  );
+}
