024edf155c401851e7c1eadce0295979e7c076bd
alissa_y <mintlol@mail.ru>
2023-10-24 17:05:28 +0300

    fix(uikit): Fix swap icon

--- icons/swap.tsx
+++ icons/swap.tsx
@@ -12,17 +12,19 @@ export default function SwapIcon(props: IconProps) {
     >
       <path
         opacity="0.8"
-        fillRule="evenodd"
-        clipRule="evenodd"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
         d="M3 16L7 20H9L13 16L11.5 14.5L9 17V8H7V17L4.5 14.5L3 16Z"
         fill="black"
+        style={{ fill: "black", fillOpacity: 1 }}
       />
       <path
         opacity="0.4"
-        fillRule="evenodd"
-        clipRule="evenodd"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
         d="M15 4L11 8L12.5 9.5L15 7V16H17L17 7L19.5 9.5L21 8L17 4H15Z"
         fill="black"
+        style={{ fill: "black", fillOpacity: 1 }}
       />
     </svg>
   );
