fe602b97d2739c07d2b9afabe2fad21e0930d406
Alex D <pr0fedt@gmail.com>
2023-10-24 11:56:06 +0200

    small refator

--- types/core.ts
+++ types/core.ts
@@ -1,6 +1,8 @@
 import type { CSSProperties, MouseEvent, ReactNode } from "react";
+import type { StyledTarget } from "styled-components/dist/types";
 
 export interface DefaultProps {
+  as?: StyledTarget<any>;
   className?: string;
   style?: CSSProperties;
   children?: ReactNode;
