612ab8f4c93d87d4bb675bf0b2658237923d92b2
Vilmir Baikov <vilmir.baykov@eqlab.io>
2023-11-13 18:23:43 +0300

    fix(ui): Superfield - fix onClick type

--- components/input/super-field.tsx
+++ components/input/super-field.tsx
@@ -39,7 +39,7 @@ const WIDTHS: WidthMap = {
   ".": 10,
 };
 
-type Props = Omit<InputHTMLAttributes<HTMLInputElement>, "value"> &
+type Props = Omit<InputHTMLAttributes<HTMLInputElement>, "value" | "onClick"> &
   Partial<ComposeProps<typeof classes, boolean>> & {
     className?: string;
     children?: ReactNode;
